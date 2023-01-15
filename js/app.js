f75867b5065f51c1d21d34e8423915b7a90a3284
Rodrigo Riveros <riverosdev@gmail.com>
2023-01-15 01:04:47 -0300

    Agrego vaciar carrito

--- js/app.js
+++ js/app.js
@@ -10,6 +10,16 @@ cargarEventListeners();
 function cargarEventListeners(){
     // Cuando agregas un curso presionando "Agregar al carrito"
     listaCursos.addEventListener("click", agregarCurso);
+
+    // Elimina cursos del carrito
+    carrito.addEventListener("click", eliminarCurso);
+
+    // Vaciar el carrito
+    vaciarCarritoBtn.addEventListener("click", () => {
+        articulosCarrito = []; // Resetea el arreglo
+
+        limpiarHTML(); // Elimina todo el HTML
+    });
 }
 
 //funciones
@@ -22,6 +32,20 @@ function agregarCurso(e){
         leerDatosCurso(cursoSeleccionado);
     }
 }
+
+// Eimina un curso del carrito
+function eliminarCurso(e) {
+    if(e.target.classList.contains("borrar-curso")) {
+        const cursoId = e.target.getAttribute("data-id");
+
+        // Elimina del arreglo de articulosCarrito por el data-id
+        articulosCarrito = articulosCarrito.filter( curso  => curso.id !== cursoId );
+
+        carritoHTML(); // Itero sobre el carrito y muestro su HTML
+    }
+}
+
+
 
 //Lee HTML y extrae info del curso
 
@@ -37,8 +61,23 @@ function leerDatosCurso(curso){
         cantidad: 1
     }
 
-    // Agrega elementos al arreglo de carrito
-    articulosCarrito = [...articulosCarrito, infoCurso];
+    // Revisa si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id );
+    if(existe) {
+        // Actualizamos la cantidad
+        const cursos = articulosCarrito.map( curso => {
+            if( curso.id === infoCurso.id ){
+                curso.cantidad++;
+                return curso; // Retorna el obj actualizado
+            } else {
+                return curso; // Retorna los obj que no son los duplicados
+            }
+        });
+        articulosCarrito = [...cursos];
+    } else {
+        // Agrega elementos al arreglo de carrito
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
 
     console.log(articulosCarrito);
 
@@ -56,10 +95,15 @@ function carritoHTML() {
     // Recorre el carrito y genera el HTML
 
     articulosCarrito.forEach( curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso;
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>
-                ${curso.titulo}
+            <td><img src="${imagen}" width="100"></td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td> ${cantidad}</td>
+            <td> 
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
             </td>
         `;
 
